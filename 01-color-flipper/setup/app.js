4b49874687f1f0e90e891374bf17085d91c36a57
glenmiracle18 <m.bonyu@alustudent.com>
2023-11-08 22:10:26 +0200

    done

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,12 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+
+btn.addEventListener('click', () => {
+    // getting a ramdom number between 0 and 3 (just int)
+    const randomNumber = Math.floor(Math.random() * colors.length);
+
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber]
+})
